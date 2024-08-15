4d6904f5291beeced5d899d4eb0f28efadb9f844
Mana <57663038+Manaball123@users.noreply.github.com>
2024-08-15 17:04:03 +0800

    cleanup and stuff

--- injector/main.cpp
+++ injector/main.cpp
@@ -4,6 +4,10 @@ using namespace Injector;
 
 
 int main(int argc, char** argv) {
+    if(argc < 2) {
+        std::cout << "Usage: injector <pid or name> <module name>";
+        return 0;
+    }
 
     pid_t pid;
     if(is_number(argv[1])) {
@@ -15,16 +19,15 @@ int main(int argc, char** argv) {
         pid = atol(string_split(res, " ")[1].c_str());
         std::cout << "Found process with pid " << pid << "\n"; 
     }
-    // if(res == 0) {
-    //     //do nothing(lol)
-    //     while(1){
-    //         sleep(1);
-    //     };
-    // }
+    std::string mod_name("bin/module.so");
+    if(argc == 3) {
+        mod_name = argv[2];
+    }
+
     //prints hello world and exits
     //credits: https://gist.github.com/procinger/a65c8bde824a10294a4a6966de5a47b4
-    byte sc[] = "\xeb\x20\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb0\x01\x40\xb7\x01\x5e\xb2\x0c\x0f\x05\x48\x31\xc0\xb0\x3c\x40\xb7\x00\x0f\x05\xe8\xdb\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21""\x48\xC7\xC0\x3C\x00\x00\x00\x0F\x05";
-    Buffer scb(sc, sizeof(sc));
+    // byte sc[] = "\xeb\x20\x48\x31\xc0\x48\x31\xff\x48\x31\xf6\x48\x31\xd2\xb0\x01\x40\xb7\x01\x5e\xb2\x0c\x0f\x05\x48\x31\xc0\xb0\x3c\x40\xb7\x00\x0f\x05\xe8\xdb\xff\xff\xff\x48\x65\x6c\x6c\x6f\x20\x57\x6f\x72\x6c\x64\x21""\x48\xC7\xC0\x3C\x00\x00\x00\x0F\x05";
+    // Buffer scb(sc, sizeof(sc));
     CInjector injector(pid);
     injector.Init();
     std::cout << "Starting inject...\n";
--- sample-process/main.cpp
+++ sample-process/main.cpp
@@ -7,11 +7,6 @@ using dlopen_t = decltype(dlopen);
 int main() {
 
     std::cout << "hello from sample process!\nPID: " << getpid() << "\n";
-    void* hdl = dlopen("/usr/lib/libc.so.6", RTLD_NOW);
-
-    dlopen_t* p_dlopen = (dlopen_t*)dlsym(hdl, "dlopen");
-    std::cout << "dlopen: " << std::hex << (void*)p_dlopen << "\n";
-    //p_dlopen("/mnt/ssd0_ext4/git/linux-process-injection/bin/module.so", RTLD_NOW);
     while(1) {
         std::cout << "update\n";
         sleep(1);
